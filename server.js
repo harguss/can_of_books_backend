c7d905bd8850647a741a1335654745ddf7796927
Leah Wilner <leahwilner1@gmail.com>
2023-02-11 15:31:35 -0600

    mongo working

--- server.js
+++ server.js
@@ -7,9 +7,22 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+const mongoose = require('mongoose');
+
+const Books = require('./models/books.js');
+
+mongoose.connect(process.env.DB_URL);
+// comsole.log;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
 const PORT = process.env.PORT || 5005;
 
-app.get('/test', (request, response) => {
+app.get('/', (request, response) => {
 
   response.send('test request received');
 
